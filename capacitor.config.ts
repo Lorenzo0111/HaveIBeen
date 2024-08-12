850a941bc1430a3d402d9b4afe9d778c0c92a6a7
Lorenzo <hello@lorenzo0111.me>
2024-08-12 12:40:09 +0200

    Added storage and skyscanner support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'me.lorenzo0111.haveibeen',
   appName: 'Have I Been?',
   webDir: 'dist'
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,33 @@
+import { Share } from "@capacitor/share";
 import {
+  IonAlert,
+  IonButton,
   IonContent,
   IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import { type Database, Storage } from "@ionic/storage";
 import { useEffect, useState } from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
+import iso from "iso-3166-1";
 
 const Home: React.FC = () => {
   const [db, setDb] = useState<Database | null>(null);
   const [visited, setVisited] = useState<string[]>([]);
+  const [selected, setSelected] = useState<{
+    name: string;
+    code: string;
+  } | null>(null);
 
   useEffect(() => {
     async function initDb() {
@@ -24,41 +40,153 @@ const Home: React.FC = () => {
     initDb();
   }, []);
 
+  useEffect(() => {
+    if (!db) return;
+
+    async function loadVisited() {
+      const visited = await db.get("visited");
+      setVisited(visited ? JSON.parse(visited) : []);
+    }
+
+    loadVisited();
+  }, [db]);
+
+  useEffect(() => {
+    if (!db) return;
+
+    async function saveVisited() {
+      await db.set("visited", JSON.stringify(visited));
+    }
+
+    saveVisited();
+  }, [db, visited]);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Have I Been?</IonTitle>
+          <IonTitle>🌏 Have I Been?</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Have I Been?</IonTitle>
+            <IonTitle size="large">🌏 Have I Been?</IonTitle>
           </IonToolbar>
         </IonHeader>
 
         <ComposableMap>
-          <Geographies
-            onClick={(e) => {
-              console.log(e);
-            }}
-            geography="https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
-          >
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  fill="var(--ion-color-primary)"
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onClick={(e) => {
-                    console.log(`Clicked on ${geo.properties.name}`);
-                  }}
-                />
-              ))
-            }
-          </Geographies>
+          <ZoomableGroup>
+            <Geographies geography="https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json">
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    fill={
+                      visited.includes(geo.properties.name)
+                        ? "var(--ion-color-primary)"
+                        : "var(--ion-text-color)"
+                    }
+                    style={{
+                      default: { outline: "none" },
+                      hover: { outline: "none" },
+                      pressed: { outline: "none" },
+                    }}
+                    key={geo.rsmKey}
+                    geography={geo}
+                    onClick={() => {
+                      setSelected({
+                        name: geo.properties.name,
+                        code: geo.id,
+                      });
+                    }}
+                  />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
         </ComposableMap>
+
+        <h2 className="text-center font-extrabold text-2xl my-3">
+          You have visited {visited.length} countries <br /> That's the{" "}
+          {Math.round((visited.length / 193) * 100)}%!
+        </h2>
+
+        <IonList>
+          {visited.map((country) => (
+            <IonItem
+              key={country}
+              onClick={() => {
+                setVisited((visited) => visited.filter((v) => v !== country));
+              }}
+            >
+              <IonLabel>{country}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+
+        <IonButton
+          className="w-full my-3"
+          onClick={() => {
+            Share.share({
+              title: "Countries I've visited",
+              text: `I've visited ${
+                visited.length
+              } countries! That's the ${Math.round(
+                (visited.length / 193) * 100
+              )}% of the world! 🌍\n\n${
+                visited.length > 0
+                  ? visited.map((item) => `- ${item}`).join("\n")
+                  : ""
+              }`,
+            });
+          }}
+        >
+          Share
+        </IonButton>
+
+        <IonAlert
+          header={selected?.name}
+          subHeader="What would you like to do?"
+          buttons={[
+            {
+              text: "✈️",
+              handler: () => {
+                if (!selected) return;
+
+                const code = iso.whereNumeric(selected.code)?.alpha2;
+                if (!code) return;
+
+                window.open(
+                  `https://www.skyscanner.it/trasporti/voli/it/${code}/?adultsv2=1&cabinclass=economy&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false`,
+                  "_blank"
+                );
+
+                setSelected(null);
+              },
+            },
+            visited.includes(selected?.name ?? "")
+              ? {
+                  text: "❌",
+                  role: "confirm",
+                  handler: () => {
+                    setVisited((visited) =>
+                      visited.filter((v) => v !== selected?.name)
+                    );
+                    setSelected(null);
+                  },
+                }
+              : {
+                  text: "✅",
+                  role: "confirm",
+                  handler: () => {
+                    setVisited((visited) => [...visited, selected?.name ?? ""]);
+                    setSelected(null);
+                  },
+                },
+          ]}
+          isOpen={!!selected}
+          onDidDismiss={() => setSelected(null)}
+        ></IonAlert>
       </IonContent>
     </IonPage>
   );
